007bb31e9b1e13de3fdc740b09dd72095a1cf8a0
EmilyDSarani <emily.sarani@gmail.com>
2021-08-13 15:37:34 -0500

    Javascript works

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
 // import functions and grab DOM elements
+const MonteupButton = document.querySelector('#mUp');
+const MontedownButton = document.querySelector('#mDown');
+const Montecounter = document.querySelector('#mCounter');
+
 
 // initialize state
+let counterM = 0;
 
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+MonteupButton.addEventListener('click', () => {
+
+    // - the variable goes UP by one
+    counterM = counterM + 7;
+    // - display the new value to the user (inject it into the div)
+    Montecounter.textContent = counterM;
+});
+
+MontedownButton.addEventListener('click', () => {
+
+    // - the variable goes UP by one
+    counterM = counterM - 7;
+    // - display the new value to the user (inject it into the div)
+    Montecounter.textContent = counterM;
+});
+
+const CapupButton = document.querySelector('#cUp');
+const CapdownButton = document.querySelector('#cDown');
+const Capcounter = document.querySelector('#cCounter');
+
+let counterC = 0;
+
+CapupButton.addEventListener('click', () => {
+
+    counterC = counterC + 7;
+    Capcounter.textContent = counterC;
+});
+
+CapdownButton.addEventListener('click', () => {
+
+    counterC = counterC - 7;
+    Capcounter.textContent = counterC;
+});
